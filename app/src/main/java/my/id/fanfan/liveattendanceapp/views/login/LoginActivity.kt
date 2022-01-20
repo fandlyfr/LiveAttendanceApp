63de70a5a3ca7c8af9b06bf8dcf231d5fdaa30b5
fandlyfr <fandlyfr@gmail.com>
2022-01-20 16:54:42 +0800

    ui: add login page

--- app/src/main/java/my/id/fanfan/liveattendanceapp/views/login/LoginActivity.kt
+++ app/src/main/java/my/id/fanfan/liveattendanceapp/views/login/LoginActivity.kt
@@ -1,12 +1,12 @@
-package my.id.fanfan.liveattendanceapp.views.splash
+package my.id.fanfan.liveattendanceapp.views.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import my.id.fanfan.liveattendanceapp.R
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
     }
 }
